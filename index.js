b02879f2472d479f79964629ffb06679f4c0de1f
Jan Kopriva <jan.kopriva@gooddata.com>
2015-10-03 22:45:51 +0200

    Import styles

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './css/skeleton';
+
 let rootElement = document.getElementById('root');
 ReactDOM.render(
   <div>Hello world!</div>,
